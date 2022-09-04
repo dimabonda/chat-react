8786dea00e9a94c90c5b73018a9f7b2429a3426d
DimaBondarenko <dimabondarenko2404@gmail.com>
2022-09-05 01:47:31 +0300

    remove AvatarComponent

--- src/components/MessageGroup/MessageGroup.jsx
+++ src/components/MessageGroup/MessageGroup.jsx
@@ -5,33 +5,22 @@ import { backendURL } from "../../helpers/gql"
 import Message from "../Message/Message"
 import { AvatarWrapper, MessageGroupWrapper, MessagesWrapper } from "./MessageGroup.style"
 
-const AvatarComponent = memo(({url}) => {
+export const MessageGroup = ({messages, chatId, currUser}) => {
     
-    console.log('url')
+    const url = messages[0]?.owner?.avatar?.url || '';
+    
     return (
-        <AvatarWrapper>
+        <MessageGroupWrapper>
+            <MessagesWrapper>
+                {messages.map((mes, i, arr) => <Message key={mes?._id || mes?.status} lastElem={i === 0 ? false : true} currUser={currUser} chatId={chatId} mes={mes}/> )}
+            </MessagesWrapper>
+            <AvatarWrapper>
             <Avatar
-                // alt={message?.nick || message?.login ||  'avatar'}
+                alt={messages[0]?.nick || messages[0]?.login ||  'avatar'}
                 src={`${backendURL}/${url}`}
                 sx={{width: 45, height: 45, mr: '20px', position: 'sticky', bottom: 0, marginBottom: '5px'}}
             />
         </AvatarWrapper>
-    )
-})
-
-export const MessageGroup = ({messages, chatId, currUser}) => {
-    
-    const url = messages[0]?.owner?.avatar?.url || '';
-
-    
-    
-    return (
-        <MessageGroupWrapper>
-            
-            <MessagesWrapper>
-                {messages.map((mes, i, arr) => <Message key={mes?._id || mes?.status} lastElem={i === 0 ? false : true} currUser={currUser} chatId={chatId} mes={mes}/> )}
-            </MessagesWrapper>
-            <AvatarComponent url={url}/>
         </MessageGroupWrapper> 
     )
 }
